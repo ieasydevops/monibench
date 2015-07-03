24c807d65b9e203d65f302cab3720edba1d7a9b9
梅新云 <xinyun.mxy@alibaba-inc.com>
2015-07-03 11:20:02 +0800

    metis

---
+++ metis/metis-common/src/main/java/org/metis/common/App.java
@@ -0,0 +1,13 @@
+package org.metis.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ metis/metis-rule/src/main/java/org/metis/rule/App.java
@@ -0,0 +1,13 @@
+package org.metis.rule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
